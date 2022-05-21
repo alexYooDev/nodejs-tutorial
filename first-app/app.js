a9069f4b491dd6b3cfefa9172d807edce25b58bc
d_flex <hiyoo95@gmail.com>
2022-05-22 00:55:17 +0900

    transitioning to no sql version using mongo db

--- first-app/app.js
+++ first-app/app.js
@@ -2,26 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars');
 
+// 9gBBaT0jYaGDmTZD
+
 const path = require('path');
 
-const sequelize = require('./util/database');
-
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const MongoConnect = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
+// const adminRouter = require('./routes/admin');
+// const shopRouter = require('./routes/shop');
 
-const errorController = require('./controllers/errors');
+// const errorController = require('./controllers/errors');
 
 //next를 호출해야 다음 미들웨어를 실행. 그렇지 않다면 미들웨어는 실행 종료한다.
 
@@ -32,50 +27,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 유저 로그인 대체
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((error) => console.log(error));
+// app.use((req, res, next) => {
+//   // User.findByPk(1)
+//   //   .then((user) => {
+//   //     req.user = user;
+//   //     next();
+//   //   })
+//   //   .catch((error) => console.log(error));
+// });
+
+// app.use('/admin', adminRouter.router);
+// app.use(shopRouter);
+
+// app.use(errorController.get404);
+
+MongoConnect((client) => {
+  console.log(client);
+  app.listen(5002);
 });
 
-app.use('/admin', adminRouter.router);
-app.use(shopRouter);
-
-app.use(errorController.get404);
-
 // 데이터 베이스 내 테이블의 관계를 정의한다.
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-// run when npm starts!
-sequelize
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      User.create({
-        name: 'Alex',
-        email: 'test@test.com',
-      });
-    }
-    return user;
-  })
-  .then((user) => {
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(5002);
-  })
-  .catch((error) => console.log(error));
--- first-app/util/database.js
+++ first-app/util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node_complete', 'root', '200209', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const MongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://alex:9gBBaT0jYaGDmTZD@cluster0.u9lrb.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then((result) => {
+      console.log('connected!!');
+      callback(result);
+    })
+    .catch((error) => console.log(error));
+};
+
+module.exports = MongoConnect;
